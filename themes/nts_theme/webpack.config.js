5c9ab3b2067d9c2fc971e6e9cf39fa14e2f5a36b
Maks Oleksyuk <maksym.oleksyuk@gmail.com>
2023-12-09 16:25:42 +0200

    Update webpack entry config

--- themes/nts_theme/webpack.config.js
+++ themes/nts_theme/webpack.config.js
@@ -1,18 +1,29 @@
 import path from 'path';
+import { glob } from 'glob';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import RemoveEmptyScriptsPlugin from 'webpack-remove-empty-scripts';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 const dirname = process.cwd();
 const isDev = process.env.NODE_ENV !== 'production';
 
-// mode: production development
+const getEntries = () =>
+  glob
+    .sync('./styles/**/*.scss')
+    .filter((file) => !path.basename(file).startsWith('_'))
+    .reduce((entries, file) => {
+      const key = path
+        .relative('./styles', file)
+        .replace(/\.(scss|css)$/, '')
+        .replace(/\\/g, '/');
+      entries[key.startsWith('style') ? `base/${key}` : key] = `./${file}`;
+      return entries;
+    }, {});
 
+// mode: production development
 export default {
   mode: 'development',
-  entry: {
-    // Base
-    'base/style': ['./styles/style.scss'],
-  },
+  entry: () => getEntries(),
   output: {
     filename: 'js/[name].js',
     path: path.resolve(dirname, 'dist'),
@@ -29,6 +40,7 @@ export default {
   },
   plugins: [
     new RemoveEmptyScriptsPlugin(),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'css/[name].css',
     }),
