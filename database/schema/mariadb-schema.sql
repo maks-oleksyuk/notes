3b7b12abed4515ce8ac12dd5cada4a4ff58d49f2
Maks Oleksyuk <maksym.oleksyuk@gmail.com>
2024-09-28 22:40:51 +0300

    Clear project files by plugin

--- database/schema/mariadb-schema.sql
+++ database/schema/mariadb-schema.sql
@@ -131,6 +131,3 @@ CREATE TABLE `users` (
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 /*!999999\- enable the sandbox mode */
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES (1,'0001_01_01_000000_create_users_table',1);
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES (2,'0001_01_01_000001_create_cache_table',1);
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES (3,'0001_01_01_000002_create_jobs_table',1);
