a5434e2cce83e0deb8e7031dffcecde2b147dd7a
Maks Oleksyuk <maksym.oleksyuk@gmail.com>
2024-12-14 15:39:36 +0200

    add request helper

---
+++ app/Helpers/Http/RequestHelper.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helpers\Http;
+
+final readonly class RequestHelper
+{
+    public static function toBoolean(mixed $possibleBoolean): ?bool
+    {
+        return filter_var($possibleBoolean, FILTER_VALIDATE_BOOL, FILTER_NULL_ON_FAILURE);
+    }
+
+    /**
+     * @return array<string>
+     */
+    public static function parseStringIntoArray(string $inputString): array
+    {
+        return array_filter(
+            array_map('trim', explode(',', $inputString)),
+            static fn ($item) => $item !== ''
+        );
+    }
+
+    /**
+     * @return array<int>
+     */
+    public static function parseStringIntoIntArray(string $inputString): array
+    {
+        return array_map(
+            'intval',
+            array_filter(
+                self::parseStringIntoArray($inputString),
+                static fn ($item) => is_numeric($item)
+            )
+        );
+    }
+}
