0b0a92432dbe3e5137e2922ae05bef345c05e2c8
Maks Oleksyuk <maksym.oleksyuk@gmail.com>
2025-03-09 12:45:20 +0200

    wip add basic auth to pulse

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,4 +6,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\RepositoryServiceProvider::class,
+    Olssonm\VeryBasicAuth\VeryBasicAuthServiceProvider::class,
 ];
