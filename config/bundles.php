98ade789f4cda7239aa0ee43e2360579ecfc6012
Maks Oleksyuk <maksym.oleksyuk@gmail.com>
2025-04-20 17:36:54 +0300

    chore: update packages

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
