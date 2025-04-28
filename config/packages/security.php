eeebd14761e6b088ddc45ee6da6cb8178ceb750a
Maks Oleksyuk <maksym.oleksyuk@gmail.com>
2025-04-28 22:08:29 +0300

    feat: change config file format from yml to php

---
+++ config/packages/security.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Entity\User;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->extension('security', [
+        'password_hashers' => [
+            PasswordAuthenticatedUserInterface::class => 'auto',
+        ],
+        'providers' => [
+            'app_user_provider' => [
+                'entity' => [
+                    'class' => User::class,
+                    'property' => 'username',
+                ],
+            ],
+        ],
+        'firewalls' => [
+            'dev' => [
+                'pattern' => '^/(_(profiler|wdt)|css|images|js)/',
+                'security' => false,
+            ],
+            'main' => [
+                'lazy' => true,
+                'provider' => 'app_user_provider',
+                'form_login' => [
+                    'login_path' => 'app_login',
+                    'check_path' => 'app_login',
+                    'enable_csrf' => true,
+                ],
+                'logout' => [
+                    'path' => 'app_logout',
+                ],
+            ],
+        ],
+        'access_control' => null,
+    ]);
+    if ('test' === $containerConfigurator->env()) {
+        $containerConfigurator->extension('security', [
+            'password_hashers' => [
+                PasswordAuthenticatedUserInterface::class => [
+                    'algorithm' => 'auto',
+                    'cost' => 4,
+                    'time_cost' => 3,
+                    'memory_cost' => 10,
+                ],
+            ],
+        ]);
+    }
+};
---
+++ config/packages/web_profiler.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    if ('dev' === $containerConfigurator->env()) {
+        $containerConfigurator->extension('web_profiler', [
+            'toolbar' => true,
+        ]);
+        $containerConfigurator->extension('framework', [
+            'profiler' => [
+                'collect_serializer_data' => true,
+            ],
+        ]);
+    }
+
+    if ('test' === $containerConfigurator->env()) {
+        $containerConfigurator->extension('framework', [
+            'profiler' => [
+                'collect' => false,
+            ],
+        ]);
+    }
+};
