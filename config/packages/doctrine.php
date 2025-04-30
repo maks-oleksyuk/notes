ef577b118eb9d35781d9af53994881bc6792b986
Maks Oleksyuk <maksym.oleksyuk@gmail.com>
2025-04-30 10:00:18 +0300

    feat: update doctrine config

--- config/packages/doctrine.php
+++ config/packages/doctrine.php
@@ -4,72 +4,75 @@ declare(strict_types=1);
 
 use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Config\Doctrine\Orm\EntityManagerConfig\MappingConfig;
+use Symfony\Config\DoctrineConfig;
+use Symfony\Config\FrameworkConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension('doctrine', [
-        'dbal' => [
-            'url' => '%env(resolve:DATABASE_URL)%',
-            'profiling_collect_backtrace' => '%kernel.debug%',
-            'use_savepoints' => true,
-        ],
-        'orm' => [
-            'auto_generate_proxy_classes' => true,
-            'enable_lazy_ghost_objects' => true,
-            'report_fields_where_declared' => true,
-            'validate_xml_mapping' => true,
-            'naming_strategy' => 'doctrine.orm.naming_strategy.underscore_number_aware',
-            'identity_generation_preferences' => [
-                PostgreSQLPlatform::class => 'identity',
-            ],
-            'auto_mapping' => true,
-            'mappings' => [
-                'App' => [
-                    'type' => 'attribute',
-                    'is_bundle' => false,
-                    'dir' => '%kernel.project_dir%/src/Entity',
-                    'prefix' => 'App\Entity',
-                    'alias' => 'App',
-                ],
-            ],
-            'controller_resolver' => [
-                'auto_mapping' => false,
-            ],
-        ],
-    ]);
-    if ('test' === $containerConfigurator->env()) {
-        $containerConfigurator->extension('doctrine', [
-            'dbal' => [
-                'dbname_suffix' => '_test%env(default::TEST_TOKEN)%',
-            ],
-        ]);
-    }
+use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
 
-    if ('prod' === $containerConfigurator->env()) {
-        $containerConfigurator->extension('doctrine', [
-            'orm' => [
-                'auto_generate_proxy_classes' => false,
-                'proxy_dir' => '%kernel.build_dir%/doctrine/orm/Proxies',
-                'query_cache_driver' => [
-                    'type' => 'pool',
-                    'pool' => 'doctrine.system_cache_pool',
-                ],
-                'result_cache_driver' => [
-                    'type' => 'pool',
-                    'pool' => 'doctrine.result_cache_pool',
-                ],
-            ],
-        ]);
-        $containerConfigurator->extension('framework', [
-            'cache' => [
-                'pools' => [
-                    'doctrine.result_cache_pool' => [
-                        'adapter' => 'cache.app',
-                    ],
-                    'doctrine.system_cache_pool' => [
-                        'adapter' => 'cache.system',
-                    ],
-                ],
-            ],
-        ]);
+return static function (
+    ContainerConfigurator $containerConfigurator,
+    DoctrineConfig $doctrineConfig,
+    FrameworkConfig $frameworkConfig,
+): void {
+    $dbalConfig = $doctrineConfig->dbal();
+    $ormConfig = $doctrineConfig->orm();
+    $entityManagerConfig = $ormConfig->entityManager('default');
+
+    $dbalConfig
+        ->connection('default')
+        ->url(env('DATABASE_URL')->resolve())
+        ->profilingCollectBacktrace(param('kernel.debug'))
+        ->useSavepoints(true);
+
+    $ormConfig
+        ->autoGenerateProxyClasses(true)
+        ->enableLazyGhostObjects(true)
+        ->controllerResolver()
+        ->autoMapping(false);
+
+    $entityManagerConfig
+        ->reportFieldsWhereDeclared(true)
+        ->validateXmlMapping(true)
+        ->namingStrategy('doctrine.orm.naming_strategy.underscore_number_aware')
+        ->identityGenerationPreference(PostgreSQLPlatform::class, 'identity')
+        ->autoMapping(true);
+
+    /** @var MappingConfig $appMapping */
+    $appMapping = $entityManagerConfig->mapping('App');
+    $appMapping
+        ->type('attribute')
+        ->isBundle(false)
+        ->dir(param('kernel.project_dir').'/src/Entity')
+        ->prefix('App\Entity')
+        ->alias('App');
+
+    switch ($containerConfigurator->env()) {
+        case 'test':
+            $dbalConfig
+                ->connection('default')
+                ->dbnameSuffix('_test'.env('TEST_TOKEN')->default(''));
+
+            break;
+        case 'prod':
+            $ormConfig
+                ->autoGenerateProxyClasses(false)
+                ->proxyDir(param('kernel.project_dir').'/doctrine/orm/Proxies');
+
+            $entityManagerConfig->queryCacheDriver([
+                'type' => 'pool',
+                'pool' => 'doctrine.system_cache_pool',
+            ]);
+            $entityManagerConfig->resultCacheDriver([
+                'type' => 'pool',
+                'pool' => 'doctrine.result_cache_pool',
+            ]);
+
+            $cache = $frameworkConfig->cache();
+            $cache->pool('doctrine.system_cache_pool')->adapters(['cache.system']);
+            $cache->pool('doctrine.result_cache_pool')->adapters(['cache.app']);
+
+            break;
     }
 };
