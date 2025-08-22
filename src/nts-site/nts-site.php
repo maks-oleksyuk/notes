0edfd90f1b5fad91a283eff67aad18699ae3da29
Maks Oleksyuk <90793591+maks-oleksyuk@users.noreply.github.com>
2025-08-22 22:24:32 +0300

    Add ACF plugin (#44)

---
+++ src/nts-site/nts-site.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+Plugin Name: NTS Site
+Description: Custom functionality for Notes site.
+Version: 1.0.0
+Requires PHP: 8.4
+License: GPL-3.0-or-later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+require_once 'includes/acf/acf.php';
